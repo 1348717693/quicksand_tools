934287937226c7c41e95fa3836b0935441b8b80d
tylabs <tylermcl@gmail.com>
2016-07-29 15:17:05 -0400

    added command line options

--- quicksand_report.py
+++ quicksand_report.py
@@ -1,7 +1,15 @@
 import requests
+import argparse
 
 url = "https://www.quicksand.io/api.php"
-values = {'key': '60bb2e16f59a8e7c6b51101a79b34cd9', 'query': '42d087ac77d8bab5c418ccf0828698db4656e539d60174aa2ea5b5a802f9c4fa'}
 
+#handle command lines
+parser = argparse.ArgumentParser()
+parser.add_argument("query", help="Hash to find md5,sha1,sha256,sha512")
+parser.add_argument("-k", "--key", help="Optional API Key", default="0000000000000000000000000000000a")
+args = parser.parse_args()
+
+
+values = {'key': args.key, 'query': args.query}
 r = requests.post(url, data=values)
 print r.text
