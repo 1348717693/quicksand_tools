bc80814981621fe126b7badcd804bd028ee9a31b
tylabs <tylermcl@gmail.com>
2016-07-29 15:14:22 -0400

    added command line options

--- quicksand_api.py
+++ quicksand_api.py
@@ -1,9 +1,20 @@
 import requests
+import argparse
 
 url = "https://www.quicksand.io/upload.php"
-files = {'file[]': open('file.doc', 'rb')}
-values = {'key': '60bb2e16f59a8e7c6b51101a79b34cd9'}
-#values = {'key': '60bb2e16f59a8e7c6b51101a79b34cd9', 'unzip': 'infected1'}
+
+#handle command lines
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="File to upload for analysis")
+parser.add_argument("-k", "--key", help="Optional API Key", default="0000000000000000000000000000000a")
+parser.add_argument("-p", "--password", help="Optional Unzip Password")
+args = parser.parse_args()
+
+
+files = {'file[]': open(args.file, 'rb')}
+values = {'key': args.key}
+if args.password:
+  values['unzip'] = args.password
 
 r = requests.post(url, files=files, data=values)
 print r.text
