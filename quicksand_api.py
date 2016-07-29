b22e650121db030d6b7028607fa88d6ead6222d0
tylabs <tylermcl@gmail.com>
2016-07-29 14:42:06 -0400

    support for unzip password

--- quicksand_api.py
+++ quicksand_api.py
@@ -3,6 +3,7 @@ import requests
 url = "https://www.quicksand.io/upload.php"
 files = {'file[]': open('file.doc', 'rb')}
 values = {'key': '60bb2e16f59a8e7c6b51101a79b34cd9'}
+#values = {'key': '60bb2e16f59a8e7c6b51101a79b34cd9', 'unzip': 'infected1'}
 
 r = requests.post(url, files=files, data=values)
 print r.text
